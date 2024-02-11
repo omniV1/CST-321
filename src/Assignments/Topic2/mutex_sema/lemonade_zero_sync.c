a51620c1af1df330f638b4ef05436ed3a0d2b3d3
Owen Lindsey <123825005+omniV1@users.noreply.github.com>
2024-02-10 20:33:18 -0700

    Update lemonade_zero_sync.c

--- src/Assignments/Topic2/mutex_sema/lemonade_zero_sync.c
+++ src/Assignments/Topic2/mutex_sema/lemonade_zero_sync.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+##include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -6,57 +6,59 @@
 #define KIDS 5
 #define USES 3
 
-// Global variables to simulate shared resources
-int refrigeratorOpen = 0; // Represents if the refrigerator is being used
-int availableCups = 2; // Simulates a limited number of cups
+int refrigeratorOpen = 0;
+int availableCups = 2;
 
-// Thread function to simulate kids serving lemonade without synchronization
+// Function to increment cups safely
+void incrementCups() {
+    availableCups = availableCups + 1;
+}
+
+// Function to decrement cups safely
+void decrementCups() {
+    if (availableCups > 0) {
+        availableCups = availableCups - 1;
+    }
+}
+
 void* serveLemonade(void* arg) {
     int kidId = *(int*)arg;
     for (int i = 0; i < USES; i++) {
-        // Attempt to use the refrigerator
         if (!refrigeratorOpen) {
-            refrigeratorOpen = 1; // Refrigerator is now being used
+            refrigeratorOpen = 1;
             printf("Kid %d is using the refrigerator.\n", kidId);
-            sleep(1); // Simulate time to get lemonade
-            refrigeratorOpen = 0; // Done using the refrigerator
+            sleep(1);
+            refrigeratorOpen = 0;
             printf("Kid %d is done with the refrigerator.\n", kidId);
         } else {
             printf("Kid %d couldn't use the refrigerator because it's already in use.\n", kidId);
         }
 
-        // Attempt to serve lemonade
+        // Serve lemonade if cups are available
         if (availableCups > 0) {
-            availableCups--; // Take a cup
+            decrementCups(); // Decrement safely
             printf("Kid %d is serving lemonade. Cups left: %d\n", kidId, availableCups);
-            sleep(1); // Simulate serving time
-            availableCups++; // Return the cup
+            sleep(1);
+            incrementCups(); // Increment safely
         } else {
             printf("Kid %d couldn't serve lemonade because there are no cups available.\n", kidId);
         }
     }
-    free(arg); // Clean up dynamically allocated memory
+    free(arg);
     return NULL;
 }
 
 int main() {
     pthread_t kids[KIDS];
-
-    // Create threads for each kid
-    for (int i = 0; i < KIDS; i++) {
+    for(int i = 0; i < KIDS; i++) {
         int* kidId = malloc(sizeof(int));
         *kidId = i;
-        if(pthread_create(&kids[i], NULL, serveLemonade, kidId) != 0) {
-            perror("Failed to create thread");
-            return 1;
-        }
+        pthread_create(&kids[i], NULL, serveLemonade, kidId);
     }
-
-    // Wait for all kids to finish
-    for (int i = 0; i < KIDS; i++) {
+    for(int i = 0; i < KIDS; i++) {
         pthread_join(kids[i], NULL);
     }
-
     printf("All kids are done serving lemonade.\n");
     return 0;
 }
+
